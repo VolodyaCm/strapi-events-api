1fa18ba96a6416fdff792c4dca899525ef0570f6
Volodymyr Smal-Stotskiy <volodyacm@gmail.com>
2021-06-11 20:58:19 +0300

    Add file parser

--- api/events/controllers/events.js
+++ api/events/controllers/events.js
@@ -1,8 +1,23 @@
-'use strict';
+const { parseMultipartData, sanitizeEntity } = require('strapi-utils');
+var jf = require('jsonfile')
+const Blob = require("cross-blob");
+const moment = require('moment');
 
-/**
- * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
- * to customize this controller
- */
+module.exports = {
+  async createMany(ctx) {
+    const stringArrayBuffer = ctx.request.body.file;
+    const buf = Buffer.from(stringArrayBuffer.split(','))
+    const content = JSON.parse(buf.toString());
 
-module.exports = {};
+    const all = await Promise.all(content.events.map((event) => {
+      console.log(event.date);
+      console.log( moment(event.date).format('yyyy-MM-DD'));
+      return strapi.services.events.create({
+        ...event,
+        date: moment(event.date).format('yyyy-MM-DD')
+      });
+    }));
+
+    return all.map(entity => sanitizeEntity(entity, { model: strapi.models.events }))
+  },
+};
